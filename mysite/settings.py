d70808fa9a5730a3d8bd39fdbffbbb962b732394
arqambinrehan <arqambinrehan@gmail.com>
2019-06-22 03:01:44 +0500

    Modified templates.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jyyni#haq__h5dv83t$h6l#7t!jh5^oxtt1zp=&yh673*)1-1*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
